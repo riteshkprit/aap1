d96f3b973da4611e39e61aabb0dd10359dfe4f79
RiteshKprit <riteshkumar@kpritech.ac.in>
2024-12-01 23:08:24 +0530

    write code for sample calculator

---
+++ app.java
@@ -0,0 +1,12 @@
+public class app {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = add(a, b);
+        System.out.println("Sum: " + sum);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
